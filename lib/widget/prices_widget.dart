078889d5f326635e41fc4444f6801206d115d511
shyne <shyne9795@gmail.com>
2018-10-26 00:45:33 +0900

    add open browser button

--- lib/widget/prices_widget.dart
+++ lib/widget/prices_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scraping/model/market_price_model.dart';
 import 'package:flutter_scraping/widget/price_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PricesWidget extends StatelessWidget {
   PricesWidget(this.marketPriceModel);
@@ -16,6 +17,18 @@ class PricesWidget extends StatelessWidget {
           kind: "median", price: marketPriceModel.medianPrice.toString()),
       PriceWidget(
           kind: "average", price: marketPriceModel.averagePrice.toString()),
+      RaisedButton(
+        child: Text("open browser"),
+        onPressed: () => _launchURL(marketPriceModel.targetSiteUrl),
+      )
     ]);
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
